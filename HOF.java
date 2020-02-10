2f31d2c0564ffb1afdc6df842991a207c6053bee
houseoflavor <60902243+houseoflavor@users.noreply.github.com>
2020-02-10 17:41:43 -0500

    Add files via upload
    from class 2/10

---
+++ HOF.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Color.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class HOF extends JPanel{
+
+   public HOF(){
+   
+   }
+   
+   public void paintComponent(Graphics g){
+   
+   }
+
+
+
+}
---
+++ HOFDriver.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Graphics;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+import java.io.*;
+
+public class HOFDriver{
+
+   public static HOF window;
+   
+   public static void main(String[] args){
+      window = new HOF();
+      JFrame w = new JFrame("House of Flavor");
+      w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      w.setContentPane(window);
+      w.setLocation(0, 0);
+      HOF graphics = window;
+      w.setSize(1300,745);
+      w.setResizable(false); // can't resize screen
+      
+      w.setVisible(true); // show it
+
+   }
+
+}

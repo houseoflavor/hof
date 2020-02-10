851ba0ea37bb5feb436c3940cc38f5bb042de5ca
houseoflavor <houseoflavor1@gmail.com>
2020-02-10 18:23:23 -0500

    lots of stuff

--- HOF.java
+++ HOF.java
@@ -16,12 +16,49 @@ import java.util.TimerTask;
 
 public class HOF extends JPanel{
 
-   public HOF(){
-   
+   Timer timer1 = new Timer(); // refresh screen timer
+   TimerTask task1 = new TimerTask(){
+      public void run(){
+         repaint();
+      }
+   };
+
+   static String mode; // menu, credits, ingame
+   static int x1, y1, dx1, dy1;// player 1 instance ints
+   static int x2, y2, dx2, dy2; // player 2 instance ints
+
+
+   public HOF(){ // constructor for jpanel
+      
+      
+   }
+
+   public void start(){
+      timer1.scheduleAtFixedRate(task1,0,100); // delay = 0ms, period = 100ms
    }
    
-   public void paintComponent(Graphics g){
-   
+   public void paintComponent(Graphics g){ //method override for graphics
+      super.paintComponent(g);
+      this.setBackground(Color.WHITE); // set color (change if you want)
+      
+      HOFUtil.doEverything(g); // main draw
+
+      /* cancel timer (should never do because this timer draws everything)
+      timer.cancel();
+      timer.purge();
+      */
+   }
+
+   public void keyCommand(Integer key){
+      // IMPORTANT: https://stackoverflow.com/questions/15313469/java-keyboard-keycodes-list
+      if(mode.equals("ingame")){ // main chunk for in game
+         if(key == KeyEvent.VK_W){ // p1 moving up
+              
+         }
+
+
+      }
+      
    }
 
 
---
+++ HOFUtil.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Arrays.*;
+import java.util.*;
+
+public class HOFUtil extends HOF{
+    public static void doEverything(Graphics g){
+
+
+
+
+        
+    }
+
+
+}

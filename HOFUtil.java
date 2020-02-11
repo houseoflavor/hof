cad46e27f5c5bdee7f19614401634c261c22a555
houseoflavor <houseoflavor1@gmail.com>
2020-02-10 20:32:26 -0500

    stuff
    level 1
    fixed movement

--- HOFUtil.java
+++ HOFUtil.java
@@ -7,11 +7,14 @@ import java.util.*;
 
 public class HOFUtil extends HOF{
    public static void doEverything(Graphics g){
+      // background drawing
+   
+   
       // character drawing
       g.setColor(Color.RED);
-      g.fillOval((int)x1,(int)y1,50,50);
+      g.fillOval(((int)x1),((int)y1),50,50);
       g.setColor(Color.BLUE);
-      g.fillOval((int)x2,(int)y2,50,50);
+      g.fillOval(((int)x2),((int)y2),50,50);
    
    
    
